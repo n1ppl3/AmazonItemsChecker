4c87172a2571f79132099488137a7b4d3d1a182a
apashkovskiy <alexandr.pashkovskiy@shtormtech.ru>
2020-01-15 17:10:45 +0300

    Utils more methods

--- src/main/java/ru/n1ppl3/amazon/items/checker/util/Utils.java
+++ src/main/java/ru/n1ppl3/amazon/items/checker/util/Utils.java
@@ -1,15 +1,44 @@
 package ru.n1ppl3.amazon.items.checker.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.FileCopyUtils;
+import ru.n1ppl3.amazon.items.checker.dto.SellingItem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 
-
+@Slf4j
 public abstract class Utils {
 
     private static final String URL_TEMPLATE = "https://www.amazon.co.jp/gp/offer-listing/{0}/ref=dp_olp_new?ie=UTF8&condition=new";
 
-
-    public static String itemSellingPageUrl(String itemId) {
+    static String itemSellingPageUrl(String itemId) {
         return MessageFormat.format(URL_TEMPLATE, itemId);
+    }
+
+    public static SellingItem getAndParseSellingItemPage(String itemId) throws IOException {
+        String url = itemSellingPageUrl(itemId);
+        return getAndParseSellingItemPage(new URL(url));
+    }
+
+    public static SellingItem getAndParseSellingItemPage(URL url) throws IOException {
+        String content = getContent(url);
+        return parseContent(content);
+    }
+
+    private static String getContent(URL url) throws IOException {
+        logger.info("gonna get {}", url);
+        InputStream is = url.openStream();
+        byte[] bytes = FileCopyUtils.copyToByteArray(is); // is closed
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    private static SellingItem parseContent(String content) {
+        logger.info("gonna parse\n{}", content);
+        return null; // TODO FIXME
     }
 
 }
--- src/test/java/ru/n1ppl3/amazon/items/checker/util/UtilsTest.java
+++ src/test/java/ru/n1ppl3/amazon/items/checker/util/UtilsTest.java
@@ -1,8 +1,14 @@
 package ru.n1ppl3.amazon.items.checker.util;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import ru.n1ppl3.amazon.items.checker.dto.SellingItem;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class UtilsTest {
 
@@ -10,5 +16,18 @@ class UtilsTest {
     void itemSellingPageUrl() {
         assertEquals("https://www.amazon.co.jp/gp/offer-listing/B0006TPI66/ref=dp_olp_new?ie=UTF8&condition=new",
             Utils.itemSellingPageUrl("B0006TPI66"));
+    }
+
+    @Test
+    void getAndParseSellingItemPageReal() throws IOException {
+        SellingItem sellingItem = Utils.getAndParseSellingItemPage("B0006TPI66");
+        assertEquals(null, sellingItem);
+    }
+
+    @Test
+    void getAndParseSellingItemPageFromFileSystem() throws IOException {
+        Resource resource = new ClassPathResource("test-page.html");
+        SellingItem sellingItem = Utils.getAndParseSellingItemPage(resource.getURL());
+        assertEquals(null, sellingItem);
     }
 }
