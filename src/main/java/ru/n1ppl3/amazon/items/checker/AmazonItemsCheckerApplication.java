4fc6828054c299fb029e5433870a08253ee1d001
apashkovskiy <alexandr.pashkovskiy@shtormtech.ru>
2020-01-15 10:54:18 +0300

    spring initializr files

---
+++ src/main/java/ru/n1ppl3/amazon/items/checker/AmazonItemsCheckerApplication.java
@@ -0,0 +1,13 @@
+package ru.n1ppl3.amazon.items.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonItemsCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonItemsCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/n1ppl3/amazon/items/checker/AmazonItemsCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.n1ppl3.amazon.items.checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonItemsCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
